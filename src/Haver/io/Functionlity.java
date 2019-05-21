15980a42201c81effddf3273b94f0bb4d2ea76cb
radheshyam999 <radheshyamdubey999@gmail.com>
2019-05-21 12:59:51 +0530

    start working on Admin

---
+++ src/Haver/io/Functionlity.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package Haver.io;
+
+/**
+ * @author ADMIN
+ *
+ */
+public class Functionlity 
+ {
+	   
+	   private String AdminUserName;
+	   private String AdminPassword;
+	   private int PlayerId;
+	   
+	   
+//	public String getIdGame()
+//	  {
+//		 return IdGame;
+//	  }
+//	public void setIdGame(String idGame) {
+//		IdGame = idGame;
+//	}
+	public int getPlayerId() {
+		return PlayerId;
+	}
+	public void setPlayerId(int playerId) {
+		PlayerId = playerId;
+	}
+	public String getAdminPassword() {
+		return AdminPassword;
+	}
+	public void setAdminPassword(String adminPassword) {
+		AdminPassword = adminPassword;
+	}
+	public String getAdmin() {
+		return AdminUserName;
+	}
+	public void setAdmin(String admin) {
+		AdminUserName = admin;
+	}
+	   
+	   
+	   
+	
+	
+	
+
+ }
